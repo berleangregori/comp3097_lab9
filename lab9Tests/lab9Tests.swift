75d8d6c28852dd16a13c6b6f329e238bd033b071
Berlean Gregori <berlean.gregori@hotmail.com>
2025-03-20 23:34:52 -0400

    Initial Commit

---
+++ lab9Tests/lab9Tests.swift
@@ -0,0 +1,17 @@
+//
+//  lab9Tests.swift
+//  lab9Tests
+//
+//  Created by Joshua Nuezca on 2025-03-20.
+//
+
+import Testing
+@testable import lab9
+
+struct lab9Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
